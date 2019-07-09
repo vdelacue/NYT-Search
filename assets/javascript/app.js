6140f4b7fa140593bd9ee9819be710871469761f
Greg Manco <gregmanco@gmail.com>
2019-07-09 13:59:11 -0400

    added ajax call and api url key

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+
+
+var apiKey =  "key=1f8412f8-a516-467f-be58-3ec773fd36c0"
+var apiURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-" + apiKey
+
+$.ajax({
+    url: apiURL
+    method: "GET"
+})
+
+.then(function(response) {
+    console.log(response)
+
+});
