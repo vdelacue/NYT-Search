f7749d660520cd2d1203a7de29a9a9a24a6cf6ff
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-07-09 14:19:22 -0400

    syntax error

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 
 
-var apiKey =  "key=rXJVNU62LGNJWrDACV6AfbWzNQ6pUEKM"
-var apiURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-" + apiKey
+var apiKey =  "key=rXJVNU62LGNJWrDACV6AfbWzNQ6pUEKM";
+var apiURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-" + apiKey;
 
 $.ajax({
     url: apiURL,
